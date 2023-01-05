ef4c243ee278c4c6140bc422b4955dea017fe4a2
roic alessandro <29387855+AlessandroRoic@users.noreply.github.com>
2023-01-05 19:18:07 +0100

    feat: adds login

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 'use strict';
 
-(() => {})();
+(() => {
+  const appDiv = document.getElementById('app');
+  const username = document.getElementById('username');
+  const password = document.getElementById('password');
+  const form = document.getElementById('form');
+
+  const localUsername = localStorage.getItem('username');
+
+  if (localUsername) {
+    form.remove();
+    appDiv.textContent = `Welcome ${localUsername}`;
+    localStorage.clear();
+  }
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (username.value && password.value) {
+      localStorage.setItem('username', username.value);
+      form.remove();
+      appDiv.textContent = `Welcome ${username.value}`;
+    }
+  });
+})();
+
