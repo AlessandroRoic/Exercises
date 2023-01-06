134a90eeb4cdd2cb7529c9a724f5053a246fb55e
roic alessandro <29387855+AlessandroRoic@users.noreply.github.com>
2023-01-06 15:59:32 +0100

    feat: adds modal

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-'use strict';
+"use strict";
 
-(() => {})();
+(() => {
+  const handleModalClosed = (event, modal) => {
+    event.preventDefault();
+    if (
+      event.target.matches(".modal") ||
+      event.target.matches(".modal__close")
+    ) {
+      modal.classList.remove("modal--open");
+    }
+  };
+
+  const handleModalOpened = (event) => {
+    if (event.target.dataset.modal) {
+      event.preventDefault();
+      const modal = document.getElementById(event.target.dataset.modal);
+      modal.classList.add("modal--open");
+      modal.addEventListener("click", (event) =>
+        handleModalClosed(event, modal)
+      );
+    }
+  };
+
+  const init = () => {
+    document.addEventListener("click", handleModalOpened, false);
+  };
+
+  init();
+})();
