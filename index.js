da354a54ec2147223ef79ce3bbe227fb697af8c8
roic alessandro <29387855+AlessandroRoic@users.noreply.github.com>
2023-01-07 13:47:28 +0100

    feat: adds todo

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-'use strict';
+"use strict";
 
-(() => {})();
+(() => {
+  const form = document.querySelector("#todo-form");
+  const input = document.querySelector("#todo-input");
+  const todoList = document.querySelector(".todo__list");
+
+  const createItem = (label) => {
+    const todoItem = document.createElement("div");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.addEventListener("click", (e) => {
+      todoItem.className = e.target.checked ? "todo__item--checked" : "";
+    });
+    todoItem.appendChild(checkbox);
+    todoItem.append(`${label}`);
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "x";
+    deleteButton.addEventListener("click", () => {
+      todoItem.remove();
+    });
+    todoItem.append(deleteButton);
+    return todoItem;
+  };
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    todoList.append(createItem(input.value));
+    input.value = "";
+  });
+})();
