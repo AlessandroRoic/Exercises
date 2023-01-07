017d28606248bb48b3a6b592ad7b427512241cdd
roic alessandro <29387855+AlessandroRoic@users.noreply.github.com>
2023-01-07 14:29:54 +0100

    feat: adds fetch user data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { map, takeUntil } from 'rxjs/operators';
+
+interface UserData {
+  name: {
+    first: string;
+    last: string;
+  };
+  picture: {
+    medium: string;
+  };
+}
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'exercises';
+export class AppComponent implements OnInit, OnDestroy {
+  userData: UserData| null = null;
+  private onDestroy$ = new Subject<void>();
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getUserData();
+  }
+
+  ngOnDestroy(): void {
+    this.onDestroy$.next();
+    this.onDestroy$.complete();
+  }
+
+  private getUserData() {
+    this.httpClient
+      .get('https://randomuser.me/api/')
+      .pipe(
+        takeUntil(this.onDestroy$),
+        map((data: any) => data['results'][0])
+      )
+      .subscribe((data) => {
+        this.userData = data;
+        console.log(data);
+      });
+  }
 }
