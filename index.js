fb2afc7bd49cbdc7d6f8546357cc264ccb5c3e30
roic alessandro <29387855+AlessandroRoic@users.noreply.github.com>
2023-01-07 13:58:30 +0100

    feat: adds map

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-'use strict';
+"use strict";
 
-(() => {})();
+(() => {
+  const map = (inputArray, mapFunction) => {
+    let copiedArray = inputArray;
+    for (let i = 0; i < copiedArray.length; i++) {
+      if (typeof inputArray === "string") {
+        const value = mapFunction(copiedArray[i], i, inputArray);
+        copiedArray =
+          copiedArray.substring(0, i) +
+          value +
+          (i + 1 < inputArray.length ? copiedArray.substring(i + 1) : "");
+        continue;
+      }
+      copiedArray[i] = mapFunction(copiedArray[i], i, inputArray);
+    }
+    return copiedArray;
+  };
+
+  const test = map([1, 2, 3, 4], (x, index, array) => x + index + array.length);
+  const test2 = map("this is a test", (x) => (x === " " ? "" : x));
+  console.log({ test, test2 });
+})();
