64294a2b5fff526b70f7793a6ccc1d17a19ed670
roic alessandro <29387855+AlessandroRoic@users.noreply.github.com>
2023-01-07 11:07:21 +0100

    feat: adds dfs node

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 'use strict';
 
-(() => {})();
+(() => {
+  const queryTagSelector = (target) => {
+    const root = document.body;
+    return depthFistSearchTag(target, root)
+  };
+
+  const depthFistSearchTag = (target, currentNode) => {
+    if (currentNode.tagName === target.toUpperCase()) {
+      return currentNode;
+    }
+
+    for (let node of currentNode.childNodes) {
+      const result = depthFistSearchTag(target, node);
+      if (result) return result;
+    }
+
+    return null;
+  };
+
+  console.log(queryTagSelector('div'));
+})();
