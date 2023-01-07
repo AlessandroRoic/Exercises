5dfd134c78b417d76e9395db68f3db5ec1f3510e
roic alessandro <29387855+AlessandroRoic@users.noreply.github.com>
2023-01-07 14:14:56 +0100

    feat: adds debounce

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-'use strict';
+"use strict";
 
-(() => {})();
+(() => {
+  function debounce(callback, duration) {
+    let timer;
+    return (...args) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => callback(...args), duration);
+    };
+  }
+
+  const input = document.getElementById("test");
+  input.addEventListener(
+    "keydown",
+    debounce((event) => {
+      console.log(event.target.value);
+    }, 700)
+  );
+})();
