4a21fe5dd417e86bd2c60cdbb5cacfcf3c45476e
roic alessandro <29387855+AlessandroRoic@users.noreply.github.com>
2023-01-07 14:16:47 +0100

    feat: adds fetch user data

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-'use strict';
+"use strict";
 
-(() => {})();
+(() => {
+  let userData = null;
+
+  const getUserData = async () => {
+    return await fetch("https://randomuser.me/api/")
+      .then((response) => response.json())
+      .then((data) => data.results)
+      .catch((error) => console.log(error));
+  };
+
+  const mapTextContent = (id, content) =>
+    (document.getElementById(id).textContent = content);
+
+  const mapUserData = (data) => {
+    userData = data[0];
+    const { name, email, picture } = userData;
+    mapTextContent("userName", `${name.first} ${name.last}`);
+    mapTextContent("email", email);
+    document.querySelector("img").src = picture.medium;
+    console.log(userData);
+  };
+
+  const init = () => {
+    getUserData().then(mapUserData);
+  };
+
+  init();
+})();
