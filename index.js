b71adf98d2def0caa76b3b4ca2a3f6a0ea2a46ca
roic alessandro <29387855+AlessandroRoic@users.noreply.github.com>
2023-01-07 11:11:03 +0100

    feat: adds filter

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 'use strict';
 
-(() => {})();
+(() => {
+  const test1 = [1,2,3,4];
+
+  const customFilter = (array, callBack) => {
+    let filteredArray = [];
+    for (let i = 0; i < array.length; i ++) {
+      if (!callBack(array[i], i, array)) continue;
+      filteredArray.push(array[i])
+    }
+    return filteredArray;
+  }
+
+  console.log(test1.filter((value) => value === 3))
+  console.log(customFilter(test1, (value) => value === 3))
+  console.log(test1.filter((value) => value > 1))
+  console.log(customFilter(test1, (value) => value > 1))
+  console.log(['1', 2, '3', 13432].filter((v) => typeof v === 'string'))
+  console.log(customFilter(['1', 2, '3', 13432],((v) => typeof v === 'string')))
+})();
