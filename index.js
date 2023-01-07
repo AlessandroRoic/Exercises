ecf4222c0769981637b1090d30e00da0063dac27
roic alessandro <29387855+AlessandroRoic@users.noreply.github.com>
2023-01-07 14:14:10 +0100

    feat: adds clone deep

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 'use strict';
 
-(() => {})();
+(() => {
+  const cloneDeep = (object) => {
+    const clonedObject = {};
+    for (const property in object) {
+      clonedObject[property] = object[property]
+      if (typeof object[property] === 'object' && object[property] !== null) {
+        cloneDeep(object[property])
+      }
+    }
+    return clonedObject;
+  }
+
+  const x = {
+    a: 124,
+    b: {
+      c: 123,
+      d: {
+        e: () => {}
+      }
+    },
+    f: {
+      g: 'stringa'
+    }
+  }
+
+  const obj = cloneDeep(x);
+  console.log(obj)
+})();
