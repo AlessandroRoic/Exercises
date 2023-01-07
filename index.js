350b9a9ae97288de81a1720177ea070ce05eb0c9
roic alessandro <29387855+AlessandroRoic@users.noreply.github.com>
2023-01-07 13:45:38 +0100

    feat: adds reduce

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-'use strict';
+"use strict";
 
-(() => {})();
+(() => {
+  const reduce = (
+    inputArray,
+    callback = (previousValue, currentValue, currentIndex, array) => {},
+    startValue
+  ) => {
+    let previousValue = startValue;
+    for (let i = 0; i < inputArray.length; i++) {
+      const element = inputArray[i];
+      previousValue = callback(previousValue, element, i, inputArray);
+    }
+    return previousValue;
+  };
+
+  console.log([1, 2, 3].reduce((acc, value) => acc + value, 0));
+  console.log(reduce([1, 2, 3], (acc, value) => acc + value, 0));
+})();
