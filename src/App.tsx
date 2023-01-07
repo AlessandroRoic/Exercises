6d161700e5d6523d1be2ea20a4b94b046f7de161
roic alessandro <29387855+AlessandroRoic@users.noreply.github.com>
2023-01-07 14:21:08 +0100

    feat: adds fetch user data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
-import * as React from 'react';
-import './styles.css';
+import axios from "axios";
+import * as React from "react";
+import { useEffect, useState } from "react";
+import "./styles.css";
+
+interface UserData {
+  name: {
+    first: string;
+    last: string;
+  };
+  picture: {
+    medium: string;
+  };
+}
 
 export default function App() {
+  const [userData, setUserData] = useState<UserData>();
+
+  useEffect(() => {
+    axios
+      .get("https://randomuser.me/api/")
+      .then(({ data }) => setUserData(data.results[0]));
+  }, []);
 
   return (
     <div>
-
+      {userData && (
+        <>
+          <img src={userData.picture.medium} alt="profile" />
+          <p>{userData.name.first}</p>
+          <p>{userData.name.last}</p>
+        </>
+      )}
     </div>
   );
 }
