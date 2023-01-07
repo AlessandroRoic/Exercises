033996cebd2f363bb8d291563e1f04190b7de2f9
roic alessandro <29387855+AlessandroRoic@users.noreply.github.com>
2023-01-07 14:11:08 +0100

    feat: adds accordion

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-'use strict';
+"use strict";
 
-(() => {})();
+(() => {
+  const Accordion = (title, content) => {
+    const instance = document.createElement("div");
+    instance.className = "accordion__wrapper";
+    instance.innerHTML = `
+    <button class="accordion__header" id="accordion-header">
+      > ${title.toString()}
+    </button>
+    <div class="accordion__body">
+      ${content.toString()}
+    </div>
+  `;
+    const accordionHeader = instance.querySelector(".accordion__header");
+    const accordionBody = instance.querySelector(".accordion__body");
+
+    accordionHeader.addEventListener("click", () => {
+      if (accordionBody.classList.contains("hidden")) {
+        accordionBody.classList.remove("hidden");
+      } else {
+        accordionBody.classList.add("hidden");
+      }
+    });
+    return instance;
+  };
+
+  document.body.append(Accordion("first accordion", "lorem ipsum"));
+  document.body.append(Accordion("second accordion", "lorem ipsum"));
+})();
