11f387285485a7889acc9b730aca5e1da6748870
roic alessandro <29387855+AlessandroRoic@users.noreply.github.com>
2023-01-19 15:28:47 +0100

    feat: adds loader

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-'use strict';
+"use strict";
 
-(() => {})();
+(() => {
+  const loader = document.getElementById("loader");
+  const loaderSr = document.getElementById('loader-sr');
+  let progress = 0;
+
+  const init = () => {
+    loading();
+  };
+
+  const loading = () => {
+    if (progress === 100) return;
+    progress += 10;
+    loader.value = progress;
+    loaderSr.textContent = `${progress}%`;
+    setTimeout(loading, 500);
+  };
+
+  init();
+})();
