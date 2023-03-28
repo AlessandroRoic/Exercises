110b5cbf55a25af164fcdb7f120320e444913261
roic alessandro <29387855+AlessandroRoic@users.noreply.github.com>
2023-03-28 19:23:01 -0400

    feat: count bits

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 'use strict';
+// Write a Javascript function to count all the 1 bits in a byte. For example, if we have a variable
+// with a value of 255, the countBits function would return 8 (since 255 is 11111111 in binary).
+function countOneBits(number) {
+  let count = 0;
+  while (number > 0) {
+    count += number & 1;
+    number >>= 1;
+  }
+  return count;
+}
 
-(() => {})();
+console.log(countOneBits(255))
